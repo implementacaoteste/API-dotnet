e0d2257922a44fe3e0c8145e45c2d3d70385eb50
Erisvaldo Carvalho <erisvaldodesenvolvedor@gmail.com>
2024-03-19 08:44:55 -0300

    Fazendo commit para testar a chave

---
+++ Models/Cliente.cs
@@ -0,0 +1,10 @@
+namespace Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+    }
+}
