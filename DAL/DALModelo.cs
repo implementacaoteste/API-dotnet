030b99fbd7640fe06fd5a7b6b8e166da2d96ab45
Erisvaldo Carvalho <106354515+implementacaoteste@users.noreply.github.com>
2024-03-26 14:51:20 -0300

    Implementação do método Salvar

--- DAL/DALModelo.cs
+++ DAL/DALModelo.cs
@@ -2,7 +2,7 @@ namespace DAL
 {
     public class DALModelo<T> where T : class
     {
-        private readonly AppDbContext context;
+        protected readonly AppDbContext context;
         /// <summary>
         /// O context só será destruído se foi originado aqui
         /// </summary>
@@ -17,6 +17,13 @@ namespace DAL
         {
             context = _context;
             inicio = false;
+        }
+        public virtual void Salvar(T _t)
+        {
+            if ((context.Entry(_t).Property("Id").CurrentValue as int?) == 0)
+                Inserir(_t);
+            else
+                Alterar(_t);
         }
         public virtual void Inserir(T _t)
         {
@@ -52,13 +59,14 @@ namespace DAL
                 context.Add(_t);
             else
                 context.Update(_t);
-
-            context.SaveChanges();
         }
         public void Dispose()
         {
             if (inicio)
+            {
+                context.SaveChanges();
                 context.Dispose();
+            }
         }
     }
 }
