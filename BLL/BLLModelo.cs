407f8ff63700815cac0a501f89f321edd2d4273c
Erisvaldo Carvalho <erisvaldodesenvolvedor@gmail.com>
2024-03-26 17:49:30 -0300

    Início da implementação da classe BLLModelo

---
+++ BLL/BLLModelo.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using DAL;
+
+namespace BLL
+{
+    public abstract class BLLModelo<T, U> where T : class where U : DALModelo<T>
+    {
+        protected virtual void ValidarDados(T _t, bool _inserindo = true)
+        {
+            if (_t == null)
+                throw new ArgumentNullException(nameof(_t), "A entidade não pode ser nula.");
+        }
+
+        protected U DataLayer { get; set; }
+
+        public virtual void Inserir(T _t)
+        {
+            ValidarDados(_t);
+            DataLayer.Inserir(_t);
+        }
+
+        public virtual List<T> BuscarTodos()
+        {
+            return DataLayer.BuscarTodos();
+        }
+
+        public virtual T? BuscarPorId(int _id)
+        {
+            return DataLayer.BuscarPorId(_id);
+        }
+
+        public virtual void Alterar(T _t)
+        {
+            ValidarDados(_t, false);
+            DataLayer.Alterar(_t);
+        }
+
+        public virtual void Excluir(int _id)
+        {
+            DataLayer.Excluir(_id);
+        }
+    }
+}
--- BLL/GrupoUsuarioBLL.cs
+++ BLL/GrupoUsuarioBLL.cs
@@ -3,34 +3,11 @@ using Models;
 
 namespace BLL
 {
-    public class GrupoUsuarioBLL
+    public class GrupoUsuarioBLL : BLLModelo<GrupoUsuario, GrupoUsuarioDAL>
     {
-        private static void ValidarDados(GrupoUsuario _grupoUsuario, bool _estaInserindo = true)
+        public GrupoUsuarioBLL()
         {
-            if (_grupoUsuario == null)
-                throw new Exception("Informe um usuário.");
-        }
-        public void Inserir(GrupoUsuario _grupoUsuario)
-        {
-            ValidarDados(_grupoUsuario);
-            new GrupoUsuarioDAL().Inserir(_grupoUsuario);
-        }
-        public List<GrupoUsuario> BuscarTodos()
-        {
-            return new GrupoUsuarioDAL().BuscarTodos();
-        }
-        public GrupoUsuario? BuscarPorId(int _id)
-        {
-            return new GrupoUsuarioDAL().BuscarPorId(_id);
-        }
-        public void Alterar(GrupoUsuario _grupoUsuario)
-        {
-            ValidarDados(_grupoUsuario, false);
-            new GrupoUsuarioDAL().Alterar(_grupoUsuario);
-        }
-        public void Excluir(int _id)
-        {
-            new GrupoUsuarioDAL().Excluir(_id);
+            this.DataLayer = new GrupoUsuarioDAL();
         }
     }
 }
