0ef4fa1ba02927d8851b69c790b8847d8f325bab
Erisvaldo Carvalho <106354515+implementacaoteste@users.noreply.github.com>
2024-03-26 14:46:37 -0300

    Refatoração da GrupoUsuarioDAL
    Agora vai herdar a DALModelo

--- DAL/GrupoUsuarioDAL.cs
+++ DAL/GrupoUsuarioDAL.cs
@@ -1,51 +1,11 @@
-using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace DAL
 {
-    public class GrupoUsuarioDAL
+    public class GrupoUsuarioDAL : DALModelo<GrupoUsuario>
     {
-        public void Inserir(GrupoUsuario _grupoUsuario)
-        {
-            using (var context = new AppDbContext())
-            {
-                context.GrupoUsuario.Add(_grupoUsuario);
-                context.SaveChanges();
-            }
-        }
-        public List<GrupoUsuario> BuscarTodos()
-        {
-            using (var context = new AppDbContext())
-            {
-                return context.GrupoUsuario.ToList();
-            }
-        }
-        public GrupoUsuario? BuscarPorId(int _id)
-        {
-            using (var context = new AppDbContext())
-            {
-                return context.GrupoUsuario.FirstOrDefault(u => u.Id == _id);
-            }
-        }
-        public void Alterar(GrupoUsuario _grupoUsuario)
-        {
-            using (var context = new AppDbContext())
-            {
-                context.Entry(_grupoUsuario).State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-        public void Excluir(int _id)
-        {
-            using (var context = new AppDbContext())
-            {
-                var grupoUsuario = context.GrupoUsuario.Find(_id);
-                if (grupoUsuario != null)
-                {
-                    context.GrupoUsuario.Remove(grupoUsuario);
-                    context.SaveChanges();
-                }
-            }
-        }
+        public GrupoUsuarioDAL() : base() { }
+
+        public GrupoUsuarioDAL(AppDbContext context) : base(context) { }
     }
 }
